9f6cafd509f3666ea0eb36e808f73b439def21fa
James Hines <jameshines10@gmail.com>
2020-03-06 18:35:51 +0000

    Adds a for loop that will be used to generate an anagram from a larger string

--- SherlockAndAnagrams.java
+++ SherlockAndAnagrams.java
@@ -1,3 +1,12 @@
 public class SherlockAndAnagrams {
-
+  public static void main(String[] args){
+    String s = "mission";
+    int anagramSize = 1;
+    int startPos = 0;
+    
+    // create the anagram
+    for(int i = 1; i < anagramSize; i++){
+      System.out.println(s.charAt(i));
+    }
+  }
 }
